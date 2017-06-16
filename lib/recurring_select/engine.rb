3f7e2aa8b08984343dbf9cef27c8fa7cbf94bf93
David Shaw <david@ianalyst.com>
2017-06-16 14:23:03 -0400

    Update

--- lib/recurring_select/engine.rb
+++ lib/recurring_select/engine.rb
@@ -3,9 +3,9 @@ module RecurringSelect
     
     initializer "recurring_select.extending_form_builder" do |app|
     # config.to_prepare do
-      ActionView::Helpers::FormHelper.send(:include, RecurringSelectHelper::FormHelper)
-      ActionView::Helpers::FormOptionsHelper.send(:include, RecurringSelectHelper::FormOptionsHelper)
-      ActionView::Helpers::FormBuilder.send(:include, RecurringSelectHelper::FormBuilder)
+      #ActionView::Helpers::FormHelper.send(:include, RecurringSelectHelper::FormHelper)
+      #ActionView::Helpers::FormOptionsHelper.send(:include, RecurringSelectHelper::FormOptionsHelper)
+      #ActionView::Helpers::FormBuilder.send(:include, RecurringSelectHelper::FormBuilder)
     end
     
     initializer "recurring_select.connecting_middleware" do |app|
